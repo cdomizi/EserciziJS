808150f51c0377d2316ff97b7ec88d7508b12f59
cdomizi <domizicesare@gmail.com>
2022-04-01 17:45:20 +0200

    Update script.js

--- 1-4/script.js
+++ 1-4/script.js
@@ -1,6 +1,13 @@
 // Selettore con "not"
-$('button').not('#quattro').addClass("green");
+$("button").not("#quattro").addClass("green");
 
 // Metodo "empty"
 let dieci = "#dieci";
 $(dieci).empty();
+
+// Metodo "off"
+const outline = () => $("#cinque").addClass("btn btn-outline-success");
+$("button").click(outline);
+
+const undo = () => $("#cinque").off("click");
+$("#uno").hover(undo);
